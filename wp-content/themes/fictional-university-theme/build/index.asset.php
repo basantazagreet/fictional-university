0f90fea0836bac97fcd2bc62d9bd444a55d438fa
Basanta <san@materiell.com>
2023-06-08 00:00:44 +0545

    Pure Js

--- wp-content/themes/fictional-university-theme/build/index.asset.php
+++ wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'adb9ea96e080c80eaef0');
+<?php return array('dependencies' => array('jquery'), 'version' => '9dfff8277f8be13586e9');
